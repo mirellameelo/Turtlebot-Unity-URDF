c529ac7c49dcf3689a3addcb371d9b905fbc6e74
mspm <mspm@cin.ufpe.br>
2019-05-06 11:32:47 -0300

    sample scene

---
+++ turtlebot_scene/Assets/RosSharp/Scripts/keyboard_control.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboard_control : MonoBehaviour
+{
+    public float Speed;
+    // Use this for initialization
+    void Start()
+    {
+        Speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Speed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
